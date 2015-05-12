af41db68199a4adcd20d216825bd52d79565338a
Murray Cumming <murrayc@murrayc.com>
2015-05-12 12:55:14 +0200

    Add a Help scene and a navigation button to reach it.

---
+++ ios-galaxyzoo/HelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelpViewController.h
+//  ios-galaxyzoo
+//
+//  Created by Murray Cumming on 12/05/2015.
+//  Copyright (c) 2015 Murray Cumming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpViewController : UIViewController
+
+@end
