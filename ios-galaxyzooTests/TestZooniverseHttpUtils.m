80bffe379418854ba03619c4c5fd67ffd39b04a7
Murray Cumming <murrayc@murrayc.com>
2015-05-19 10:27:26 +0200

    TestZooniverseHttpUtils: Add regression test for generateContentForNameValuePairs.

--- ios-galaxyzooTests/TestZooniverseHttpUtils.m
+++ ios-galaxyzooTests/TestZooniverseHttpUtils.m
@@ -50,4 +50,30 @@
     XCTAssert([header isEqualToString:@"Basic c29tZW5hbWU6c29tZWtleTEyMw=="], @"Pass");
 }
 
+- (void)testGenerateContentForNameValuePairs {
+    //An array of ZooniverseNameValuePair:
+    NSMutableArray *nameValuePairs = [[NSMutableArray alloc] init];
+
+    [ZooniverseHttpUtils addNameValuePairToArray:nameValuePairs
+                                            name:@"classification[subject_ids][]"
+                                           value:@"504f217bc499611ea60410ed"];
+    [ZooniverseHttpUtils addNameValuePairToArray:nameValuePairs
+                                            name:@"classification[annotations][0][sloan-0]"
+                                           value:@"a-1"];
+    [ZooniverseHttpUtils addNameValuePairToArray:nameValuePairs
+                                            name:@"classification[annotations][1][sloan-1]"
+                                           value:@"a-1"];
+    [ZooniverseHttpUtils addNameValuePairToArray:nameValuePairs
+                                            name:@"classification[annotations][2][sloan-2]"
+                                           value:@"a-1"];
+    [ZooniverseHttpUtils addNameValuePairToArray:nameValuePairs
+                                            name:@"classification[annotations][3][sloan-3]"
+                                           value:@"a-0"];
+
+    NSString *content = [ZooniverseHttpUtils generateContentForNameValuePairs:nameValuePairs];
+    XCTAssert([content isEqualToString:@"classification%5Bsubject_ids%5D%5B%5D=504f217bc499611ea60410ed&classification%5Bannotations%5D%5B0%5D%5Bsloan-0%5D=a-1&classification%5Bannotations%5D%5B1%5D%5Bsloan-1%5D=a-1&classification%5Bannotations%5D%5B2%5D%5Bsloan-2%5D=a-1&classification%5Bannotations%5D%5B3%5D%5Bsloan-3%5D=a-0"],
+              @"Pass");
+
+}
+
 @end
